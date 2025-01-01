e2a8d8758b7d58e2a1cc7d5821e5e711c7c89771
jamesnvdm <103289465+jamesnvdm@users.noreply.github.com>
2025-01-01 17:59:36 +0000

    Add files via upload
    Updated JS code

--- cuisine_app/static/script.js
+++ cuisine_app/static/script.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('form').onsubmit = function() {
-        const body = 'Please return a recipe using the following parameters as guidelines';
+        const body = 'Please return a recipe using the following parameters as guidelines. Kindly format the response in HTML to enable it to display beautifully on a web page, please - and do not mention the fact that this response is formatted in HTML.';
         const comments = document.querySelector('#comments').value;
         const cuisine = document.querySelector('#cuisine')?.value;
         const experience = document.querySelector('#experience')?.value;
+        const button = document.getElementById('submitButton');
+        const resetButton = document.getElementById('resetButton');
+        const resetForm = document.getElementById('reset');
+        console.log(resetForm);
+        var resultField = document.querySelector('p');
+
+        // Text to indicate that response is loading
+        resultField.innerHTML = "Fetching your response! Please wait";
+
+        // Disable submit button whilst processing response
+        button.disabled = true;
 
         console.log({
             "body": body,
@@ -25,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
         );
 
     // Send request to Django proxy
-    fetch("http://3.8.121.237:8000/chatgpt-proxy/", {
+    fetch("http://127.0.0.1:8000/chatgpt-proxy/", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -37,7 +48,10 @@ document.addEventListener('DOMContentLoaded', () => {
             return data.text(); // Display response to the user
         })
         .then((data) => {
-            alert(data);
+            resultField.innerHTML = data.replace('```html', '');
+
+            // Allow the option to reset the page once recipe displayed
+            resetForm.hidden = false;
         })
         .catch((error) => {
             console.error("Error:", error);
@@ -48,3 +62,19 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     }
 );
+
+function refreshPage() {
+    var button = document.getElementById('submitButton');
+    var resultField = document.querySelector('p');
+    var resetForm = document.getElementById('reset');
+    var experience = document.getElementById('experience');
+    var comments = document.getElementById('comments');
+    var cuisine = document.getElementById('cuisine');
+
+    button.disabled = false;
+    resultField.innerHTML = '';
+    resetForm.hidden = true;
+    comments.value = '';
+    experience.value = 'Beginner';
+    cuisine.value = 'English';
+}
